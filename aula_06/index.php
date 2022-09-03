c2d7a0221f69886ec766e8fd314b861bd5b80eb5
bll08 <izabellasantos424@gmail.com>
2022-09-02 22:39:20 -0300

    Aula de Laço repetição PWII

---
+++ aula_06/index.php
@@ -0,0 +1,27 @@
+<?php
+
+ $i=0;
+
+
+for($i=0; $i <= 100; $i++){
+    if($i %2 == 0){
+        echo "$i <br / >" ;
+    }
+}
+ ?>
+
+
+<?php
+function soma(){
+  
+
+  $nota = array();
+  $nota[0] = 8;
+  $nota[1] = 2;
+  $nota[2] = 9;
+  $nota[3] = 1;
+}
+
+echo soma();
+  
+  ?>
---
+++ aula_06/vetor.php
@@ -0,0 +1,16 @@
+
+
+ <?php
+function soma(){
+  
+
+  $nota = array();
+  $nota[0] = 8;
+  $nota[1] = 2;
+  $nota[2] = 9;
+  $nota[3] = 1;
+}
+
+echo soma();
+  
+  ?>
